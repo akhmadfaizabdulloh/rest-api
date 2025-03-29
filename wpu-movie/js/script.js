658f74522415116cc2481713ab079a4c2842c185
fafaiez <akhmadfaizabdulloh@gmail.com>
2025-03-30 06:21:38 +0700

    video 5 - Public API / Open API

---
+++ wpu-movie/js/script.js
@@ -0,0 +1,64 @@
+function searchMovie() {
+    
+    // agar data yang sudah ada di hilangkan dulu (jika ada),
+    // data terbaru tidak ada di bawahnya
+    $('#movie-list').html(''); 
+
+    $.ajax({
+        url:'http://www.omdbapi.com/',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': '1d2ff74f',
+            'i': 'tt3896198',
+            's': $('#search-input').val()
+        },
+        success: function (result) {
+            console.log(result);
+            if (result.Response == "True") {
+                let movies = result.Search;
+                // console.log(movies);
+
+                $.each(movies, function (i, data) {
+                    $('#movie-list').append(`
+                        <div class="col-md-4">
+                            <div class="card mb-3">
+                                <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">`+ data.Title +`</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">` +data.Year+`</h6>
+                                    <a href="#" class="card-link">See Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+                });
+
+                // setelah sukses menampilkan data, kita kosognkan search input-nya
+
+
+            } else {
+                // pakai backtick ( ` sebelah angka 1) untuk menulis script html
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">` + result.Error + `</h1>
+                    </div>
+                `)
+            }
+        }
+    });
+}
+
+$('#search-button').on('click', function () {
+    searchMovie();
+});
+
+$('#search-input').on('keyup', function (e) {
+    // Code Keyboard : https://keycode.info/
+    // *sekarang https://www.toptal.com/developers/keycode
+    
+    // keyCode atau which
+    if (e.keyCode === 13) {
+        searchMovie();
+    }
+});
